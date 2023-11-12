e828631c6f147fa0bbe849f7bd71eebff29cd2cd
Diego Campos <camposbuenodiego1@gmail.com>
2023-11-12 13:11:26 -0500

    "Modularizado, Header y footer terminados"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./catalogo-libros/catalogo-libros.module').then((m) => m.CatalogoLibrosModule) },
+  { path: 'registrar', loadChildren: () => import('./registrar-libros/registrar-libros.module').then((m) => m.RegistrarLibrosModule) },
+  { path: 'historial', loadChildren: () => import('./historial-solicitudes/historial-solicitudes.module').then((m) => m.HistorialSolicitudesModule) },
+  { path: 'gestionar', loadChildren: () => import('./gestionar-solicitudes/gestionar-solicitudes.module').then((m) => m.GestionarSolicitudesModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
